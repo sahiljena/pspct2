c3a224a0aacc8966445ba0621eaf5e6163dc0c32
Sahil Jena <68604369+sahiljena@users.noreply.github.com>
2020-12-09 11:39:31 +0530

    Update loader.js

--- loader.js
+++ loader.js
@@ -2,10 +2,13 @@
 document.onreadystatechange = function() { 
             if (document.readyState !== "complete") { 
                 document.querySelector("body").style.visibility = "hidden"; 
-                document.querySelector("#loader").style.visibility = "visible"; 
+                document.querySelector("#loader").style.visibility = "visible";
+                document.querySelector("#parent-loader").style.display = "visible";
             } else { 
                 document.querySelector("#loader").style.display = "none";
                 document.querySelector("#parent-loader").style.display = "none";
+                document.querySelector("#parent-loader").style.height = "0vh";
+                document.querySelector("#parent-loader").style.width = "0vw";
                 document.querySelector("body").style.visibility = "visible"; 
             } 
         };
